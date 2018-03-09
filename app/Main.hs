f0ecc3223a1e3535a859bcced1569e209d57896a
Edward Wastell <ed@wastell.co.uk>
2018-03-09 13:57:46 +0000

    Started moving to game of life

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RecordWildCards       #-}
 
@@ -8,6 +10,7 @@ module Main where
 import           DependentGrid.Class
 import           DependentGrid.Coord
 import           DependentGrid.Coord.HardWrap
+import           DependentGrid.Coord.NoOver
 import           DependentGrid.Coord.Periodic
 import           DependentGrid.Grid
 
@@ -16,7 +19,9 @@ import           Control.Comonad
 import           Control.Comonad.Store
 import           Control.Lens
 import           Control.Monad.Random
+import           Control.Monad.Zip
 import           Data.AffineSpace
+import           Data.Foldable                (toList)
 import           Data.Singletons
 import qualified Data.Vector                  as V
 import           Graphics.Rasterific
@@ -24,61 +29,48 @@ import           Graphics.Rasterific.Texture
 import           System.Random
 
 data CellState
-    = On
-    | Off
+    = Alive
+    | Dead
     deriving (Eq, Show, Ord)
 
-cellStateFromBool True  = On
-cellStateFromBool False = Off
+cellStateFromBool True  = Alive
+cellStateFromBool False = Dead
 
-rule30 :: [CellState] -> CellState
-rule30 [On, On, On]    = Off
-rule30 [On, On, Off]   = Off
-rule30 [On, Off, On]   = Off
-rule30 [Off, Off, Off] = Off
-rule30 _               = On
+gameOfLife ::
+     ( IsCoord a
+     , IsCoord b
+     , MonadZip f
+     , GetByIndex f Int
+     , MakeSized f
+     , AllDiffEq '[ a, b] Int
+     , AffineSpace a
+     , AffineSpace b
+     )
+  => FocusedGrid '[ a, b] f CellState
+  -> FocusedGrid '[ a, b] f CellState
+gameOfLife =
+  let helper g =
+        let alive = Alive == extract g
+            neigh =
+              filter (== Alive) $
+              map (`peek` g) $ mooreNeighborhood (1 :: Int) (pos g)
+        in if alive
+             then if length neigh < 2 || length neigh > 3
+                    then Dead
+                    else Alive
+             else if length neigh == 3
+                    then Alive
+                    else Dead
+  in extend helper
 
-type World = Grid '[Periodic 300] []
-type FocusedWorld a = FocusedGrid '[Periodic 300] [] a
+showGrid :: (Foldable f, Functor f) => Grid '[a,b] f CellState ->  String
+showGrid = unlines . toList . fmap (toList . fmap helper) . collapseGrid
+  where helper Alive = '#'
+        helper Dead  = '.'
 
-randomWorld :: MonadRandom m => m (World CellState)
-randomWorld = sequenceA $ pure (cellStateFromBool <$> getRandom)
+randomState ::
+     (MakeSized f, MonadZip f, Traversable f, AllAmountPossibleKnowNat cs, MonadRandom m, SingI cs)
+  => m (Grid cs f CellState)
+randomState = sequenceA $ pure (cellStateFromBool <$> getRandom)
 
-startCenter :: World CellState
-startCenter = pure Off & gridIndexed (AddCoord (Periodic 150) EmptyCoord) .~ On
-
-stepRule :: ([CellState] -> CellState) -> FocusedWorld CellState -> CellState
-stepRule rule g =
-    rule $ map (\c -> g ^. focusedGrid . gridIndexed c) $ mooreNeighborhood 1 $
-    pos g
-
-displayWorld :: FocusedWorld CellState -> String
-displayWorld =
-    let helper On  = '#'
-        helper Off = '.'
-    in collapseGrid . view focusedGrid . fmap helper
-
-run :: Int -> World CellState -> [World CellState]
-run n =
-    map (view focusedGrid) .
-    take n . iterate (extend $ stepRule rule30). makeFocusGrid
-
-data DrawInfo = DrawInfo {squareSize :: Float} deriving (Eq,Show)
-
-drawRow :: DrawInfo -> Int -> World CellState -> Drawing px ()
-drawRow DrawInfo {..} rn w =
-    let helper [x] On =
-            fill $
-            rectangle
-                (V2 (x * squareSize) (fromIntegral rn * squareSize))
-                squareSize
-                squareSize
-        helper _ _ = mempty
-    in ifoldMap (helper . fmap fromIntegral . coordAsList) w
-
-makeImage =
-    writePng "test.png" . renderDrawing 600 600 (PixelRGBA8 255 255 255 255) .
-    withTexture (uniformTexture $ PixelRGBA8 255 0 0 255) .
-    mconcat . zipWith (drawRow (DrawInfo 2)) [0 ..]
-
-main = makeImage $ run 300 startCenter
+main = undefined
